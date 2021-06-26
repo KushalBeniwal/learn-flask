d762954dade923bb93a2a429b266e5874a03ada5
Kushal Beniwal <kbeniwal2305@gmail.com>
2021-06-26 14:30:28 +0530

    run black

--- smile_cook/resources/recipe.py
+++ smile_cook/resources/recipe.py
@@ -9,7 +9,7 @@ class RecipeListResource(Resource):
     def get(self):
 
         data = []
-        
+
         for recipe in recipe_list:
             if recipe.is_publish is True:
                 data.append(recipe.data)
